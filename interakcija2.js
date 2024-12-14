d4991496eebefab40af126a8b5fa1bbb372d11d3
anavi27 <101186148+anavi27@users.noreply.github.com>
2024-12-14 16:31:23 +0100

    Update interakcija2.js
    izmjena

--- interakcija2.js
+++ interakcija2.js
@@ -1,19 +1,3 @@
-var tablinks = document.getElementsByClassName("tab-link");
-var tabcontents = document.getElementsByClassName("tab-sadrzaj");
-
-function opentab(tabname) {
-    for (tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
-
 var sidemenu = document.getElementById("sidemenu");
 
 function openmenu() {
